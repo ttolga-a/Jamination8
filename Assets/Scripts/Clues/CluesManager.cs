10c4a75a00417a872c08eb4ab5cabca0c2d4c63f
ttolga-a <tolga9507@gmail.com>
2025-10-19 13:05:30 +0300

    faaf

--- Assets/Scripts/Clues/CluesManager.cs
+++ Assets/Scripts/Clues/CluesManager.cs
@@ -4,20 +4,58 @@ using UnityEngine.UI;
 
 public class CluesManager : MonoBehaviour
 {
-    public static CluesManager instance;
+    public static CluesManager instance { get; private set; }
 
     [Header("WireCLue")]
     [SerializeField] private Image clueShowLoc;
     [SerializeField] private Sprite[] wireClueSprites;
-    public bool isWireKnown = false;
+    public bool hasFoundWireClue { get; private set; }
+
+    [Header("SymbolClue")]
+    [SerializeField] private Image[] symbolLocations;
+    [SerializeField] private Sprite[] symbolSprites;
+    public bool hasFoundSymbolClue { get; private set; }
+
+    [Header("SequenceClue")]
+    [SerializeField] private Image[] seqLocations;
+    [SerializeField] private Sprite[] seqSprites;
+    public bool hasFoundSequenceClue { get; private set; }
 
     private void Awake()
     {
-        instance = this;
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 
     public void SetupWireClueUI(int id)
     {
         clueShowLoc.sprite = wireClueSprites[id];
+    }
+
+
+
+    public void MarkWireClueAsFound()
+    {
+        if (!hasFoundWireClue)
+            hasFoundWireClue = true;
+    }
+
+    public void MarkSymbolClueAsFound()
+    {
+        if (!hasFoundSymbolClue)
+            hasFoundSymbolClue = true;
+    }
+
+    public void MarkSequenceClueAsFound()
+    {
+        if (!hasFoundSequenceClue)
+            hasFoundSequenceClue = true;
     }
 }
